81af47e6a17c4b8c86dc2aedbe47d22e89ce7107
Marek Wyszyński <45630165+gitgeoman@users.noreply.github.com>
2023-11-11 12:08:19 +0100

    chore: moving url paths

--- firstblood/urls.py
+++ firstblood/urls.py
@@ -23,8 +23,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path("", include("mathematics.urls"))
-
-    path('', views.home),
-    path('hello/<name>/', views.return_name),
-    path('mathematics/<operation>/<int:a>/<int:b>', views.calculator),
 ]
--- mathematics/urls.py
+++ mathematics/urls.py
@@ -1,8 +1,7 @@
+from django.urls import path
+from . import views
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-
-    path("", include("mathematics.urls"))
-
     path('', views.home),
     path('hello/<name>/', views.return_name),
     path('mathematics/<operation>/<int:a>/<int:b>', views.calculator),
